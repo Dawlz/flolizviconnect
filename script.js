5cacd054e66730d44910f78ffa351f0c1b9ed33d
Dawlz <37235288+Dawlz@users.noreply.github.com>
2022-07-05 16:14:04 +0100

    Form added

---
+++ script.js
@@ -0,0 +1,43 @@
+$(function () {
+	let lineCount = 1;
+	let nextId = 1;
+
+	$('.creations__plan-event-button').click(function () {
+		$('.request-modal').toggleClass('request-modal--hidden');
+		$('body').toggleClass('modal-open');
+	});
+
+	$('.creations__quote-button').click(function () {
+		$('.order-modal').toggleClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
+	});
+	$('.order-modal__header__close').click(function () {
+		$('.order-modal').addClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
+	});
+
+	$('.order-modal__overlay').click(function () {
+		$('.order-modal').addClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
+	});
+
+	$('.add-line').click(function (e) {
+		e.preventDefault();
+
+		$('.line-item').first().clone().prop('id', `line-${nextId}`).appendTo('.order-modal__item-selection');
+		$('.order-modal__item-selection').append($('.add-line'));
+		lineCount++;
+		nextId++;
+	});
+
+	$(document).on('click', '.line-item__remove', function (e) {
+		e.preventDefault();
+
+		if (lineCount > 1) {
+			const lineId = $(this).parent().prop('id');
+			$(`.line-item#${lineId}`).remove();
+
+			lineCount--;
+		}
+	});
+});
